927cb31ef1fa01dc49c9ea712953c2a6f4b81087
Justin N <2tidetwo@gmail.com>
2024-09-10 09:00:17 +0200

    added reset, colors and font

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import './App.css'
+import './styles/reset.css'
+import './styles/global.css'
 
 function App() {
-	return <></>
+	return (
+		<>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa blanditiis nihil modi ducimus
+			ipsam consequuntur eum quaerat error! Cum ipsam deleniti veritatis modi. Beatae dolorem ipsum
+			in, quidem voluptatum dicta.
+		</>
+	)
 }
 
 export default App
